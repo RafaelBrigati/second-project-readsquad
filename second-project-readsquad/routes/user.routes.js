1845a6dc989216c4455ba7029b20d69ba5e8b163
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-06-22 17:24:57 +0200

    Read Squad Project

--- second-project-readsquad/routes/user.routes.js
+++ second-project-readsquad/routes/user.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET bookspage */
-router.get("/books", (req, res, next) => {
-  res.render("books");
+router.get("/user", (req, res, next) => {
+  res.render("User");
 });
 
 module.exports = router;
