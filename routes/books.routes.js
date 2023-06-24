323e5cdb16769dd355fc0b002461257fced7575b
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-06-24 14:08:55 +0200

    Books Page

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -2,15 +2,14 @@ const express = require('express');
 const axios = require('axios')
 const router = express.Router();
 
-// app.set('view engine', 'hbs') 
-// app.set('views', __dirname + '/views')
-
 
 router.get('/books',(req, res)=> {
                     axios.get('https://openlibrary.org/people/mekBot/books/currently-reading.json')
-                    .then( response => console.log(response.data))
-                    .then(allBooks => {
-                        res.render('books', { allBooks }); 
+                
+                    .then( response => {
+                        console.log(response.data.reading_log_entries[0].work);
+                        res.render('books', { allBooks: response.data.reading_log_entries })
+                        
                     })
                     .catch(err => { console.log(err)});
 });
